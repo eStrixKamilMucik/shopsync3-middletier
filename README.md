315076b2501cb306b8d61e2d07411b656171139e
kamilmuc <kamil.mucik-prestataire@docaposte.fr>
2024-05-12 10:33:38 +0200

    Init commit

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # shopsync3-middletier
 Middle tier project
+
+# Docker
+
+## Build
+
+./mvnw spring-boot:build-image -Dspring-boot.build-image.imageName=kamilmucik/shopsync3-middletier:0.0.1-SNAPSHOT
---
+++ src/test/java/pl/estrx/ss3/middletier/MiddletierApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.estrx.ss3.middletier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiddletierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
